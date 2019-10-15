c4e45a87d2f2bb8110ac2296b9d45f2d649327bc
Juan Vega <Juan.Vega@tranzact.net>
2019-10-15 15:48:48 -0500

    Adding files

---
+++ src/SqlDeployer.CmdClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Castle.Windsor;
+using Richiban.CommandLine;
+
+namespace SqlDeployer.CmdClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = new WindsorContainer();
+
+            var config = CommandLineConfiguration.GetDefault();
+
+            config.ObjectFactory = container.Resolve;
+
+            CommandLine.Execute(config, args);
+        }
+    }
+}
